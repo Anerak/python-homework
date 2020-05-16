dfc491d17af1ccff1c778bf53689698e1271384b
Anerak <tobiastorresmdp@gmail.com>
2020-05-16 00:44:04 -0300

    Added comment

--- number-led-style.py
+++ number-led-style.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+
+# Each index contains a the representation of a number (0-9)
 digits = [
         "###\n# #\n# #\n# #\n###",
         "  #\n  #\n  #\n  #\n  #",
@@ -12,11 +14,11 @@ digits = [
         "###\n# #\n###\n  #\n###"
     ]
 
-def nums(numero):
-    numero = list(numero)
+def nums(num):
+    num = list(num)
     nxtPrnt = ""
     for i in range(5):
-        for j in numero:
+        for j in num:
             nxtPrnt += " " + digits[int(j)].split('\n')[i]
         print(nxtPrnt)
         nxtPrnt = ""
