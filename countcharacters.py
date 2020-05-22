a554831d06d89c190d3b5b9ec934c2e765d45ee3
Anerak <tobiastorresmdp@gmail.com>
2020-05-22 19:13:58 -0300

    lowercase input

--- countcharacters.py
+++ countcharacters.py
@@ -8,6 +8,7 @@ try:
         for i in file.read():
             for j in i:
                 if (j == "\n") or (j.isspace()): continue
+                else: j = j.lower()
                 if j in dict: dict[j] += 1
                 else: dict[j] = 1
 except IOError as Error: print("Error: ", strerror(Error.errno))
