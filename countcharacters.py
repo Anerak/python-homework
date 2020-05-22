f25e9f9599cd2e853524d1942c0ebc3b86b519d7
Anerak <tobiastorresmdp@gmail.com>
2020-05-22 19:01:49 -0300

    use input the related .txt file

---
+++ countcharacters.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from os import strerror
+
+dict = {}
+inp = input("Name of the file: ")
+try:
+    with open(inp, 'r') as file:
+        for i in file.read():
+            for j in i:
+                if (j == "\n") or (j.isspace()): continue
+                if j in dict: dict[j] += 1
+                else: dict[j] = 1
+except IOError as Error: print("Error: ", strerror(Error.errno))
+
+for k in dict:
+    print(f"{k} -> {dict[k]}")
